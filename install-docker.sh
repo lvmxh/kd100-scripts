c322994b0bfe0bd8f74f038651a94c5491d6a818
stack <stack@ip-172-16-1-62.us-west-1.compute.internal>
2017-11-15 18:04:36 +0000

    refine docker install

--- install-docker.sh
+++ install-docker.sh
@@ -4,9 +4,20 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+USER=stack
+
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
 docker version
+
+usermod -aG docker $USER
+iptables -P FORWARD ACCEPT
+cat <<EOF >/etc/docker/daemon.json
+{
+  "live-restore": true,
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
