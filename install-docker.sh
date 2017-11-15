277747c0b78f687322fda2edc08d505a306f69cc
U-reza-pc\reza <rroodsari@mirantis.com>
2017-11-15 07:49:17 -0800

    add docker installation

---
+++ install-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update -y
+apt-get install -y docker-engine
+
