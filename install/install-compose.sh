1ffeb6af492493f21a69f76ef84c4eb69a1e16f1
stack <stack@ip-172-16-1-202.us-west-1.compute.internal>
2017-11-16 02:12:17 +0000

    fixed install-compose.sh

--- install/install-compose.sh
+++ install/install-compose.sh
@@ -5,4 +5,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 curl -s -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose version
 
