d273dcc43f5fdefe231fa4e53a82eec334b6a15f
rezroo <rroodsari@mirantis.com>
2017-11-16 02:07:40 +0000

    Added  docker-compose install script

---
+++ install/install-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+curl -s -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
