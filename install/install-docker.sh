374956c078dd3390c47cd62679f29f41e8bd6cde
Reza Roodsari <rroodsari@mirantis.com>
2018-01-12 21:21:32 +0000

    install-docker.sh now works for xenial or trusty - gets latest community release. May have to be changed again if a specific version is needed

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -10,7 +10,9 @@ apt-get install -y apt-transport-https ca-certificates curl software-properties-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
-apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
+#apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
+#apt-get install -y docker-ce=17.09
+apt-get install -y docker-ce
 docker version
 
 usermod -aG docker $USER
